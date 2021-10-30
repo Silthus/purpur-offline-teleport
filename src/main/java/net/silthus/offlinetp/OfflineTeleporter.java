2861c9cac62a96984a98e573c6c8f61f97907b42
Silthus <michael@reichenbach.in>
2021-10-30 22:02:31 +0200

    fix(cmd): add world and offlineplayer auto completions

--- src/main/java/net/silthus/offlinetp/OfflineTeleporter.java
+++ src/main/java/net/silthus/offlinetp/OfflineTeleporter.java
@@ -4,11 +4,16 @@ import co.aikar.commands.PaperCommandManager;
 import kr.entree.spigradle.annotations.PluginMain;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.generator.WorldInfo;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @PluginMain
 public class OfflineTeleporter extends JavaPlugin {
@@ -32,6 +37,8 @@ public class OfflineTeleporter extends JavaPlugin {
     public void onEnable() {
 
         commandManager = new PaperCommandManager(this);
+        commandManager.getCommandCompletions().registerAsyncCompletion("worlds", context -> Bukkit.getWorlds().stream().map(WorldInfo::getName).collect(Collectors.toSet()));
+        commandManager.getCommandCompletions().registerAsyncCompletion("offlineplayers", context -> Arrays.stream(Bukkit.getServer().getOfflinePlayers()).parallel().map(OfflinePlayer::getName).collect(Collectors.toSet()));
         commandManager.registerCommand(new TeleportCommands(this));
     }
 }
